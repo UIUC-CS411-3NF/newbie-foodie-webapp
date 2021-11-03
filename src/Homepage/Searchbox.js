474a26ac1f6737602eddb4d531261b4ff283362c
TiehChu <tiehchu2@illinois.edu>
2021-11-02 22:37:20 -0500

    Update Searchbox

--- src/Homepage/Searchbox.js
+++ src/Homepage/Searchbox.js
@@ -2,44 +2,38 @@ import React from 'react';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Autocomplete from '@mui/material/Autocomplete';
-import { flexbox } from '@mui/system';
+import { IconButton, InputBase } from '@mui/material';
+import { SearchOutlined } from '@mui/icons-material';
+import { Box } from '@mui/system';
 
 const Searchbox = () => (
-  <div
-  style={{
-    width: '100%',
-    height: 600,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    alignContent: 'center'
-  }}
-  >
-  <Stack spacing={2} sx={{ width: '40%', }}>
-  <Autocomplete
-  id="homepage-searchbox"
-  options={top100Films.map((option) => option.title)}
-  renderInput={(params) => (
-    <TextField
-    {...params}
-    label="Search what you like to eat Today!"
-    InputProps={{
-      ...params.InputProps,
-      type: 'search',
-    }}
-    />
-    )}
-    />
-    </Stack>
+    <div
+      style={{
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignContent: 'center'
+      }}
+    >
+      <Box
+        sx={{
+          borderRadius: 8,
+          border: '1px solid grey',
+          width: 360,
+          display: 'flex',
+        }}
+      >
+        <InputBase
+          sx={{ml: 1, flex: 1, }}
+          placeholder="Search what you like to eat today!"
+        />
+        <IconButton type="submit" sx={{ p: '10px', }}>
+          <SearchOutlined />
+        </IconButton>
+      </Box>
     </div>
-    
-    );
-    
-    const top100Films = [
-      { title: 'The Shawshank Redemption'},
-      { title: 'The Godfather'},
-      { title: 'Monty Python and the Holy Grail'},
-    ];
-    
-    export default Searchbox;
-    
+  );      
+  export default Searchbox;
+  
