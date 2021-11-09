23df35fdb85385f55f6779ec5dab313adde4045e
TiehChu <tiehchu2@illinois.edu>
2021-11-09 01:48:44 -0600

    finish searchbox

--- src/Homepage/Searchbox.js
+++ src/Homepage/Searchbox.js
@@ -1,38 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Autocomplete from '@mui/material/Autocomplete';
 import { IconButton, InputBase } from '@mui/material';
-import { SearchOutlined } from '@mui/icons-material';
+import { SearchOutlined, SettingsOverscanOutlined } from '@mui/icons-material';
 import { Box } from '@mui/system';
+import { useSelector, useDispatch } from 'react-redux';
+import { getDishNameRecipesAsync } from '../features/recipe/recipeSlice';
 
-const Searchbox = () => (
-  <div
-    style={{
-      width: '100%',
-      height: '100%',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      alignContent: 'center',
-    }}
-  >
-    <Box
-      sx={{
-        borderRadius: 8,
-        border: '1px solid grey',
-        width: 360,
+const Searchbox = () => {
+  const [keyword, setKeyword] = useState('');
+  const dispatch = useDispatch();
+  const handleSearch = (event) => {
+    event.preventDefault();
+    dispatch(getDishNameRecipesAsync(keyword));
+  };
+
+  return (
+    <div
+      style={{
+        width: '100%',
+        height: '100%',
         display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignContent: 'center',
       }}
     >
-      <InputBase
-        sx={{ ml: 1, flex: 1 }}
-        placeholder="Search what you like to eat today!"
-      />
-      <IconButton type="submit" sx={{ p: '10px' }}>
-        <SearchOutlined />
-      </IconButton>
-    </Box>
-  </div>
-);
+      <Box
+        sx={{
+          borderRadius: 8,
+          border: '1px solid grey',
+          width: 360,
+          display: 'flex',
+        }}
+      >
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Search what you like to eat today!"
+          onChange={(e) => (setKeyword(e.target.value))}
+        />
+        <IconButton
+          type="submit"
+          sx={{ p: '10px' }}
+          onClick={handleSearch}
+        >
+          <SearchOutlined />
+        </IconButton>
+      </Box>
+    </div>
+  );
+};
 export default Searchbox;
